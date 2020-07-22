3d4d77a8a588172a4aa87313a29239b2dc16d8d6
kengsophy <kengsophy4@gmail.com>
2020-07-22 16:20:03 +0700

    Second Commit

---
+++ app/src/main/java/com/example/kengsophy/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.kengsophy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.nfc.cardemulation.HostNfcFService;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/kengsophy/categoriesFragment.java
@@ -0,0 +1,6 @@
+package com.example.kengsophy;
+
+import androidx.fragment.app.Fragment;
+
+public class categoriesFragment extends Fragment {
+}
